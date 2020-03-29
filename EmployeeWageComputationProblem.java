727f9bda39cc5d239ac5e26065d1d7ae9d120a7c
rajchaudhari09 <rajchaudhari366@gmail.com>
2020-03-29 11:55:48 +0530

    ADDED: Calculating employee wage

--- EmployeeWageComputationProblem.java
+++ EmployeeWageComputationProblem.java
@@ -2,21 +2,30 @@
 // Employee Wage Computation Problem In Java:-
 //Check Employee is Present or Absent:-
 //							I) Use ((RANDOM)) for Attendance Check
+//Calculate Daily Employee Wage:-
+//							I) Assume Wage per Hour is 20
+//						  II) Assume Full Day Hour is 8
 public class EmployeeWageComputationProblem
 {
 	public static void main(String[] args)
 	{
-	//Constant
+		//Constant
 		int isFullTime=1;
-	//Computation
+		int employeeRatePerHour=20;
+		//Variables
+		int employeeHours=0;
+		int employeeWage=0;
+		//Computation
 		double employeeCheck = Math.floor(Math.random() * 10) % 2;
 		if(employeeCheck == isFullTime)
 		{
-			System.out.println("Employee is the Present");
+			employeeHours=8;
 		}
 		else
 		{
-			System.out.println("Employee is not the Present");
+			employeeHours=0;
+			employeeWage=employeeHours *  employeeRatePerHour;
+			System.out.println("EmployeeWage: " +employeeWage);
 		}
 	}
 }
