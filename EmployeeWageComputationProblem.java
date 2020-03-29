de1e8460b12b6d0215f251b5a651550099c06aa7
rajchaudhari09 <rajchaudhari366@gmail.com>
2020-03-29 12:19:56 +0530

    ADDED: Add part time employee

--- EmployeeWageComputationProblem.java
+++ EmployeeWageComputationProblem.java
@@ -5,27 +5,34 @@
 //Calculate Daily Employee Wage:-
 //							I) Assume Wage per Hour is 20
 //						  II) Assume Full Day Hour is 8
+//Add Part Time Employee & Wage:-
+//							I)	Assume Part Time Hour is 8
 public class EmployeeWageComputationProblem
 {
 	public static void main(String[] args)
 	{
 		//Constant
-		int isFullTime=1;
-		int employeeRatePerHour=20;
+		int isFullTime = 1;
+		int isPartTime = 2;
+		int employeeRatePerHour = 20;
 		//Variables
-		int employeeHours=0;
-		int employeeWage=0;
+		int employeeHours = 0;
+		int employeeWage = 0;
 		//Computation
-		double employeeCheck = Math.floor(Math.random() * 10) % 2;
-		if(employeeCheck == isFullTime)
-		{
-			employeeHours=8;
-		}
+		double employeeCheck = Math.floor(Math.random() * 10) % 3;
+		if (employeeCheck == isPartTime)
+
+			employeeHours = 4;
+
+		else if (employeeCheck == isFullTime)
+
+			employeeHours = 8;
+
 		else
-		{
-			employeeHours=0;
-			employeeWage=employeeHours *  employeeRatePerHour;
-			System.out.println("EmployeeWage: " +employeeWage);
-		}
+
+			employeeHours = 0;
+			employeeWage = employeeHours * employeeRatePerHour;
+			System.out.println("EmployeeWage: " + employeeWage);
+
 	}
 }
