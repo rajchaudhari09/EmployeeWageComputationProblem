74f56e68e8c11029139fbddd8a20232aa0a35bca
rajchaudhari09 <rajchaudhari366@gmail.com>
2020-03-29 12:38:47 +0530

    ADDED: Calculating employee wage using switch

--- EmployeeWageComputationProblem.java
+++ EmployeeWageComputationProblem.java
@@ -1,36 +1,30 @@
 //!/bin/bash -x
-// Employee Wage Computation Problem In Java:-
-//Check Employee is Present or Absent:-
-//							I) Use ((RANDOM)) for Attendance Check
-//Calculate Daily Employee Wage:-
-//							I) Assume Wage per Hour is 20
-//						  II) Assume Full Day Hour is 8
-//Add Part Time Employee & Wage:-
-//							I)	Assume Part Time Hour is 8
+// Employee Wage Computation Problem In Java:=
+
 public class EmployeeWageComputationProblem
 {
+	public static final int isFullTime = 2;
+	public static final int isPartTime = 1;
+	public static final int employeeRatePerHour = 20;
+
 	public static void main(String[] args)
 	{
-		//Constant
-		int isFullTime = 1;
-		int isPartTime = 2;
-		int employeeRatePerHour = 20;
 		//Variables
 		int employeeHours = 0;
 		int employeeWage = 0;
 		//Computation
-		double employeeCheck = Math.floor(Math.random() * 10) % 3;
-		if (employeeCheck == isPartTime)
-
-			employeeHours = 4;
-
-		else if (employeeCheck == isFullTime)
-
-			employeeHours = 8;
-
-		else
-
-			employeeHours = 0;
+		int employeeCheck = (int) Math.floor(Math.random() * 10) % 3;
+		switch (employeeCheck)
+	   {
+ 			case isPartTime:
+									employeeHours = 4;
+									break;
+			case isFullTime:
+									employeeHours = 8;
+									break;
+			default:
+									employeeHours = 0;
+		}
 			employeeWage = employeeHours * employeeRatePerHour;
 			System.out.println("EmployeeWage: " + employeeWage);
 
