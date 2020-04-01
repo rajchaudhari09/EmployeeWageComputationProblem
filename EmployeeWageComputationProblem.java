f4b23b419f0b35d58e0948749a2493a5786acce6
rajchaudhari09 <rajchaudhari366@gmail.com>
2020-04-01 18:37:43 +0530

    REFACTORED: Create new class and declaring the array

--- EmployeeWageComputationProblem.java
+++ EmployeeWageComputationProblem.java
@@ -5,13 +5,65 @@ public class EmployeeWageComputationProblem
 	public final int isFullTime = 2;
 	public final int isPartTime = 1;
 
+	public int calculateWage(EmployeeWageComputationProblemNew employee)
+	{
+		//Variables
+		int employeeHours = 0;
+		int totalEmployeeHours = 0;
+		int totalWorkingDays = 0;
+		//Computation
+		while ( totalEmployeeHours <= employee.getmaximumHoursInMonth() && totalWorkingDays < employee.getnumerOfWorkingDays() )
+		{
+			totalWorkingDays++;
+			int employeeCheck =(int) ( Math.random() * 10 ) % 3;
+			switch (employeeCheck)
+	   		{
+ 				case isPartTime:
+							employeeHours = 4;
+							break;
+				case isFullTime:
+							employeeHours = 8;
+							break;
+				default:
+							employeeHours = 0;
+			}
+
+		  	 totalEmployeeHours += employeeHours;
+         	         System.out.println("Day::=" + totalWorkingDays + " employeeHours is::  " + employeeHours);
+
+		}
+
+	        employee.settotalEmployeeW(totalEmployeeHours * employee.getemployeeRatePerHour() );
+		System.out.println("total Wage of the " + employee.getcompany() + " employee is::  " + employee.gettotalEmployeeW() );
+		return totalEmployeeHours;
+	}
+
+	public static void main(String[] args)
+	{
+
+	 EmployeeWageComputationProblem employeeWageComputationProblem  = new EmployeeWageComputationProblem();
+	
+	 EmployeeWageComputationProblemNew[] EWCPN = new  EmployeeWageComputationProblemNew[2];
+	 EWCPN[0] = new EmployeeWageComputationProblemNew("Dmart",20,10,90);
+	 employeeWageComputationProblem.calculateWage(EWCPN[0]);
+
+	 EWCPN[1] = new EmployeeWageComputationProblemNew("Bigbazaar",20,60,90);
+	 employeeWageComputationProblem.calculateWage(EWCPN[1]);
+
+
+	}
+}
+
+class EmployeeWageComputationProblemNew
+{
 	public String company;
 	public int employeeRatePerHour;
 	public int numerOfWorkingDays;
 	public int maximumHoursInMonth;
-	public int totalEmployeeW;
+	public int totalEmployeeW=0;
 
-	public EmployeeWageComputationProblem(String company, int employeeRatePerHour, int numerOfWorkingDays, int maximumHoursInMonth)
+
+	public EmployeeWageComputationProblemNew( String company, int employeeRatePerHour, int numerOfWorkingDays, int maximumHoursInMonth )
 	{
 		this.company = company;
 		this.employeeRatePerHour = employeeRatePerHour;
@@ -39,53 +91,14 @@ public class EmployeeWageComputationProblem
 	{
 		return numerOfWorkingDays;
 	}
-
+	public void settotalEmployeeW( int totalEmployeeW )
+	{
+	 this.totalEmployeeW=totalEmployeeW;
+	}
 	public int gettotalEmployeeW()	
 	{
 		return totalEmployeeW;
 	}
-
-	public int calculateWage()
-	{
-		//Variables
-		int employeeHours = 0;
-		int totalEmployeeHours = 0;
-		int totalWorkingDays = 0;
-		//Computation
-		while ( totalEmployeeHours <= getmaximumHoursInMonth() && totalWorkingDays < getnumerOfWorkingDays() )
-		{
-			totalWorkingDays++;
-			int employeeCheck =(int) Math.floor(Math.random() * 10) % 3;
-			switch (employeeCheck)
-	   		{
- 				case isPartTime:
-							employeeHours = 4;
-							break;
-				case isFullTime:
-							employeeHours = 8;
-							break;
-				default:
-							employeeHours = 0;
-			}
-
-		  	 totalEmployeeHours += employeeHours;
-         	         System.out.println("Day::=" + totalWorkingDays + " employeeHours is::  " + employeeHours);
-
-		}
-
-	        totalEmployeeW = totalEmployeeHours * employeeRatePerHour;
-		System.out.println("total Wage of the " + company + " employee is::  " + totalEmployeeW);
-		return totalEmployeeHours;
-	}
-
-	public static void main(String[] args)
-	{
-
-	 EmployeeWageComputationProblem dMart = new EmployeeWageComputationProblem("dMart", 10, 30, 20);
-	 dMart.calculateWage();
-	 EmployeeWageComputationProblem bigBazaar = new EmployeeWageComputationProblem("bigBazaar", 8, 30, 30);
-	 bigBazaar.calculateWage();
-
-	}
 }
+
 
