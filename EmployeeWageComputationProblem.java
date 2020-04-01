1475574e1b167159fbf1a15500cbaef07f642db3
rajchaudhari09 <rajchaudhari366@gmail.com>
2020-04-01 20:58:38 +0530

    ADDED: Use the scanner package and the total employee wage by company

--- EmployeeWageComputationProblem.java
+++ EmployeeWageComputationProblem.java
@@ -1,5 +1,6 @@
 // Employee Wage Computation Problem In Java:=
 import java.util.ArrayList;
+import java.util.Scanner;
 
 interface EmployeeWageMethod
 {
@@ -41,13 +42,12 @@ public class EmployeeWageComputationProblem implements EmployeeWageMethod
 		  	 totalEmployeeHours += employeeHours;
 			 employeeDailyWage = employeeHours * employee.getemployeeRatePerHour();
 			 employeeDailyAndTotalWage.add( employeeDailyWage );
-         	         System.out.println("Day::=" + totalWorkingDays + " employeeHours is::  " + employeeHours);
 
 		}
+
 		employeeTotalWage = totalEmployeeHours * employee.getemployeeRatePerHour();
 		employeeDailyAndTotalWage.add( employeeTotalWage );
 	        employee.settotalEmployeeW( employeeTotalWage );
-		System.out.println("total Wage of the " + employee.getcompany() + " employee is::  " + employee.gettotalEmployeeW() );
 		return totalEmployeeHours;
 	}
 
@@ -56,13 +56,45 @@ public class EmployeeWageComputationProblem implements EmployeeWageMethod
 
 	 EmployeeWageComputationProblem employeeWageComputationProblem  = new EmployeeWageComputationProblem();
 
-	ArrayList<EmployeeWageComputationProblemNew> EWCPN = new  ArrayList<EmployeeWageComputationProblemNew>();
+	 ArrayList<EmployeeWageComputationProblemNew> EWCPN = new  ArrayList<EmployeeWageComputationProblemNew>();
 	 EWCPN.add(new EmployeeWageComputationProblemNew("Dmart",20,10,90));
 	 employeeWageComputationProblem.calculateWage(EWCPN.get(0));
 
 	 EWCPN.add(new EmployeeWageComputationProblemNew("Bigbazaar",20,60,90));
 	 employeeWageComputationProblem.calculateWage(EWCPN.get(1));
 
+	 while( true )
+	 {
+		int Choice = 0;
+			System.out.println("::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::");
+			System.out.println(":::::::::::::::::::::::::::WELCOME TO THE EMPLOYEEWAGE::::::::::::::::::::::::::::::::");
+			System.out.println("::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::");
+			System.out.println("Choice To The Employee For a Specific Company::=");
+			System.out.println("1.Dmart");
+			System.out.println("2.BigBazaar");
+			System.out.println("3.Exit");
+			System.out.println("::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::");
+			Scanner sc = new Scanner(System.in);
+			Choice = sc.nextInt();
+			if( Choice == 1 )
+			{
+				System.out.println( "The Total Employee Wage Dmart is: " + EWCPN.get(0).gettotalEmployeeW() );
+			}
+			else if( Choice == 2 )
+			{
+				System.out.println( "The Total Employee Wage BigBazaar is: " + EWCPN.get(1).gettotalEmployeeW() );
+			}
+			else if( Choice == 3 )
+			{
+				System.exit(0);
+			}
+			else
+			{
+				System.out.println("Invalid The Input \n Please Try Again Letter");
+			}
+			System.out.println("::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::");
+			System.out.println();
+	}
 
 	}
 }
